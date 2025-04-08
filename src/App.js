77aae95d06558fe749137e17708d5a972bb0c304
Ryvolution <ry4nchen@yahoo.co.uk>
2025-04-08 17:43:00 +0100

    Refactored framework and added images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className="sidebar">
+      <div className="menu top-menu">
+        <div className="menu-item">Home</div>
+        <div className="menu-item">TopMons</div>
+      </div>
+      <div className="menu bottom-menu">
+        <div className="menu-item">Options</div>
+        <div className="menu-item">About</div>
+      </div>
     </div>
+    <div className="main-content">
+      <div className="playmat">
+        <div className="deck top-left" style={{
+          backgroundImage: "url('/images/prototype_topmons_card_design_nb.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}></div>
+        <div className="deck bottom-right" style={{
+          backgroundImage: "url('/images/prototype_topmons_kai_card_design_nb.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}></div>
+          <div className="play-area">
+            <div className="opponent-row">
+              <div className="card opponent-card">Opponent Hand</div>
+            </div>
+            <div className="middle-row">
+              <div className="middle card">Player Compare</div>
+              <div className="middle card">Opponent Compare</div>
+            </div>
+            <div className="player-row">
+              <div className="card player-card">Player Hand</div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
   );
 }
 
